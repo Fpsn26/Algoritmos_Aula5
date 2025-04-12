023b8f991a5b2693706a2e8db26cfd373c1069b2
Felipe Sanches Nunes <164009006+Fpsn26@users.noreply.github.com>
2025-04-12 11:55:15 -0300

    Add files via upload

---
+++ Algoritmo380/Capicua.java
@@ -0,0 +1,19 @@
+package Algoritmo380;
+
+public class Capicua {
+
+    public static boolean ehCapicua(int numero) {
+        
+        String numeroStr = String.valueOf(numero);
+        
+        int comprimento = numeroStr.length();
+        
+        for (int i = 0; i < comprimento / 2; i++) {
+            if (numeroStr.charAt(i) != numeroStr.charAt(comprimento - 1 - i)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
